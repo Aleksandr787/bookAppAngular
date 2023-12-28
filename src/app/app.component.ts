e02a681c14b93d4b9e97dbbc5a2e46995a9e6c49
Aleksandr787 <shuverov.aleksandr@gmail.com>
2023-12-29 00:24:39 +0300

    master: navigation drawer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
 import { MatListModule } from '@angular/material/list';
 
 interface INavigationItem {
@@ -17,7 +18,8 @@ interface INavigationItem {
     CommonModule,
     RouterOutlet,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    MatIconModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -27,20 +29,49 @@ export class AppComponent {
 
   public navLinks: INavigationItem[] = [
     {
-      id: 'first',
-      label: 'First Element',
-      icon: 'home'
+      id: '1',
+      label: 'Inbox',
+      icon: 'inbox'
     },
     {
-      id: 'second',
-      label: 'Second Element',
-      icon: 'home'
+      id: '2',
+      label: 'Outbox',
+      icon: 'send'
     },
     {
-      id: 'third',
-      label: 'Third Element',
-      icon: 'home'
+      id: '3',
+      label: 'Favorites',
+      icon: 'Favorite'
+    },
+    {
+      id: '4',
+      label: 'Trash',
+      icon: 'delete'
     }
   ];
+
+  public navLinksLabels: INavigationItem[] = [
+    {
+      id: '5',
+      label: 'Label',
+      icon: 'Fiber_manual_record'
+    },
+    {
+      id: '6',
+      label: 'Label',
+      icon: 'change_history'
+    },
+    {
+      id: '7',
+      label: 'Label',
+      icon: 'stop'
+    },
+    {
+      id: '8',
+      label: 'Label',
+      icon: 'stop'
+    }
+  ];
+
   public activeLinkId: string = 'first';
 }
